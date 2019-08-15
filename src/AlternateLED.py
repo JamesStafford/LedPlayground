206e51408b616b14302ad22e898237a8f5f2f868
James Stafford <jwstafford1991@gmail.com>
2019-08-14 19:51:24 -0500

    Better implementation of blinking light that will alternate by button press

--- src/AlternateLED.py
+++ src/AlternateLED.py
@@ -1,81 +1,44 @@
-import threading
 import RPi.GPIO as GPIO
 import time
-import signal
 
-blueLedOut = 17
-redLedOut = 27
+redLedOut = 17
+yellowLedOut = 27
 buttonIn = 22
 
-redLedBlinking = True
+activeLED = yellowLedOut
 
 
 def setup():
     GPIO.setmode(GPIO.BCM)
 
-    GPIO.setup(blueLedOut, GPIO.OUT)
     GPIO.setup(redLedOut, GPIO.OUT)
+    GPIO.setup(yellowLedOut, GPIO.OUT)
 
     GPIO.setup(buttonIn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
-def blink(pin, light_on):
-    GPIO.output(pin, light_on)
-    time.sleep(.1)
-
-
-def blink_red_light():
-    light_on = True
-    while True:
-        if redLedBlinking:
-            blink(redLedOut, light_on)
-            light_on = not light_on
-        else:
-            GPIO.output(redLedOut, False)
-
-
-def blink_blue_light():
-    light_on = True
-    while True:
-        if not redLedBlinking:
-            blink(blueLedOut, light_on)
-            light_on = not light_on
-        else:
-            GPIO.output(blueLedOut, False)
-
-
-def blinking():
-    """
-    Red and Blue LED will both blinking
-    :return:
-    """
-    red_led_thread = threading.Thread(target=blink_red_light)
-    blue_led_thread = threading.Thread(target=blink_blue_light)
-    red_led_thread.start(); blue_led_thread.start()
-
-
 def console_button_press(buttonIn):
-    """
-    When button is pressed, "Button Pressed" will be print the console
-    """
+    global activeLED
     print("Button Pressed")
-    global redLedBlinking
-    redLedBlinking = not redLedBlinking
+    GPIO.output(activeLED, False)
+    if activeLED == yellowLedOut:
+        activeLED = redLedOut
+    else:
+        activeLED = yellowLedOut
 
 
 def main():
     try:
-        print("Starting LED show")
-
         setup()
         GPIO.add_event_detect(buttonIn, GPIO.FALLING, callback=console_button_press, bouncetime=500)
-        blinking()
+        light_on = True
         while True:
-            signal.pause()
-
-        print("End of LED Show")
+            GPIO.output(activeLED, light_on)
+            time.sleep(.25)
+            light_on = not light_on
     finally:
         GPIO.cleanup()
 
 
-main()
+if __name__ == '__main__':
+    main()
